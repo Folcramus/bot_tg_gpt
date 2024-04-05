f58a5e03a1d1bcd0d527f3297ca17176a0ef9fe1
Folcramus <ponopaytov@Gmail.com>
2024-04-05 11:22:29 +0300

    Добавлена команда botGPT для изменения струкутуры сообщений

--- gpt.py
+++ gpt.py
@@ -5,10 +5,12 @@ import logging
 import asyncio
 import os
 from openai import OpenAI
+import openai
 from dotenv import load_dotenv, find_dotenv
+
 load_dotenv(find_dotenv())
 assistant_id = os.getenv("ASSISTANT_ID")
-proxy =  httpx.Proxy(url=os.getenv("IP"), auth= (os.getenv("LOGIN"), os.getenv("PASS")))
+proxy = httpx.Proxy(url=os.getenv("IP"), auth=(os.getenv("LOGIN"), os.getenv("PASS")))
 client = OpenAI(api_key=os.getenv("API_KEY"),
                 http_client=httpx.Client(proxy=proxy, timeout=6000))
 
@@ -33,10 +35,11 @@ def check_status(run_id, thread_id):
         thread_id=thread_id,
         run_id=run_id,
     )
+
     return run.status
 
 
-def Messaging(mess):
+def messaging(mess: str) -> str:
     my_run_id, my_thread_id = create_thread(assistant_id, mess)
     status = check_status(my_run_id, my_thread_id)
     while status != "completed":
@@ -47,3 +50,17 @@ def Messaging(mess):
     )
     if response.data:
         return response.data[0].content[0].text.value
+
+
+def command_gpt(content: str) -> str:
+    mess  = "Структурируй содержание  данного  сообщения по красоте: " + content
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": mess,
+            }
+        ],
+        model="gpt-4-turbo-preview",
+    )
+    return chat_completion.choices[0].message.content
--- main.py
+++ main.py
@@ -3,15 +3,17 @@ import sys
 import logging
 import asyncio
 import os
+from aiogram import F
 import json
 from dotenv import load_dotenv, find_dotenv
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.filters import CommandStart, CommandObject, Command
-from gpt import Messaging
+from gpt import messaging, command_gpt
 from openai import OpenAI
 import gpt
 import re
 from aiogram.enums import ParseMode
+
 load_dotenv(find_dotenv())
 bot = Bot(os.getenv("TOKEN"), parse_mode='Markdown')
 storage = MemoryStorage()
@@ -20,21 +22,28 @@ dp = Dispatcher(storage=storage)
 
 @dp.message(CommandStart())
 async def start_command(message: types.Message):
-    res = Messaging("Привет")
+    res = messaging("Привет")
+    await message.answer(res)
+
+
+@dp.message(F.text, Command("botGPT"))
+async def command_sender(message: types.Message):
+    res = command_gpt(message.text)
+
     await message.answer(res)
 
 
 @dp.message()
 async def Sender(message: types.Message):
-    res = Messaging(message.text)
+    res = messaging(message.text)
 
     await message.answer(res)
+
+
 
 @dp.message()
 async def main() -> None:
     await dp.start_polling(bot)
-
-
 
 
 if __name__ == '__main__':
